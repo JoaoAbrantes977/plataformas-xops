291006105b038c8f60e64ce27c0897da55b0b046
João Abrantes <101903253+JoaoAbrantes977@users.noreply.github.com>
2023-12-10 03:30:45 +0000

    added tests

---
+++ server.test.js
@@ -0,0 +1,20 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('./server.js'); 
+
+// Configurar o uso do chaiHttp
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+// Testes
+describe('App', () => {
+  it('deve responder com "Hello from App Engine!" ao aceder à rota "/"', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.text).to.equal('Hello from App Engine!');
+        done();
+      });
+  });
+});
